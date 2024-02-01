f00909104b9e571be9ff5bf4b79eaa5179682ab8
DamianNaggi <147349468+DamianNaggi@users.noreply.github.com>
2024-02-01 00:51:19 +0000

    Itterating through arrays

--- src/App.java
+++ src/App.java
@@ -1,6 +1,28 @@
+import java.util.Arrays;
+
 public class App {
   
   public static void main(String[] args) {
-    
+      int[] arr = new int[] {92,12,38,74};
+      for (int i = 0; i < arr.length; i++){
+        System.out.println(arr[i]);
+    }
+    System.out.println();
+
+  for (int i = arr.length - 1; i>=0; i--) {
+    System.out.println(arr[i]);
+  }
+System.out.println();
+
+for (int i = arr.length - 1; i >= 0; i = i - 2) {
+  System.out.println(arr[i]);
+}
+
+System.out.println();
+
+Arrays.stream(arr).forEach(System.out::println);
+System.out.println();
+
+
   }
 }
